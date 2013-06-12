614b5e308d05c384b31cee2f347c49d85456a9f2
Jais Cheema <jais@trikeapps.com>
2013-06-12 16:46:21 +1000

    modifications for rails3

--- view_steps.rb
+++ view_steps.rb
@@ -5,27 +5,27 @@ Then /^I should be forwarded to "([^\"]*)"$/ do |path|
 end
 
 Then /^I should see the "([^\"]*)" link$/ do |link_title|
-  response.should have_tag("a", :text => link_title)
+  page.should have_selector("a", :text => link_title)
 end
 
 Then /^I should not see the "([^\"]*)" link$/ do |link_title|
-  response.should_not have_tag("a", :text => link_title)
+  page.should_not have_selector("a", :text => link_title)
 end
 
 Then /^I should see the "([^\"]*)" link to (.*)$/ do |link_title, link_page|
-  response.should have_tag("a[href=#{path_to(link_page)}]", :text => link_title)
+  page.should have_link(link_title, href: path_to(link_page))
 end
 
 Then /^I should see a "([^\"]*)" button$/ do |title|
   if title =~ /^Choose File$/
-    response.should have_tag("input[type=file]")
+    page.should have_xpath("//input[@type='file']")
   else
-    response.should have_tag("*[value=#{title}]")
+    page.should have_xpath("//*[@value='#{title}']")
   end
 end
 
 Then /^I should not see a "([^\"]*)" button$/ do |title|
-  response.should_not have_tag("*[value=#{title}]")
+  page.should_not have_xpath("*[@value='#{title}']")
 end
 
 Then /^I should see a "([^"]*)" select$/ do |id|
